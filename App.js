6599b858656a0dcea966e10b7a07c0586e172215
Varalakshmi2808 <168578332+Varalakshmi2808@users.noreply.github.com>
2024-07-19 00:07:33 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -5,10 +5,10 @@ let App=()=>{
     "https://th.bing.com/th?id=OIP.pAcu7875Nl6e_M0tBXBzGQHaE5&w=307&h=203&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2",
     "https://tse4.mm.bing.net/th/id/OIP.anp0XQz24UeOEE5qf-5swQHaEo?w=298&h=186&c=7&r=0&o=5&dpr=1.5&pid=1.7",
     "https://tse4.mm.bing.net/th/id/OIP.rjExmgmKTbfTBdhiO6pcZAHaE8?w=292&h=195&c=7&r=0&o=5&dpr=1.5&pid=1.7",
-    "https://tse3.mm.bing.net/th/id/OIP.zBTFCq6IB11YUgBOc7c1mwHaEK?w=309&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7"
+    "https://tse3.mm.bing.net/th/id/OIP.zBTFCq6IB11YUgBOc7c1mwHaEK?w=309&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7","https://tse2.mm.bing.net/th/id/OIP.wg4R0mAD1_DQAII9hCM-8AHaDk?w=331&h=168&c=7&r=0&o=5&dpr=1.5&pid=1.7","https://tse4.mm.bing.net/th/id/OIP.byenSyw81CvjeS10wk49sgHaET?w=319&h=185&c=7&r=0&o=5&dpr=1.5&pid=1.7"
   ]
   let h=["image-1","image-2","image-3","image-4","image-5"]
-  let [i,setI]=useState(0)
+  let [i,setI]=useState(1)
   let inc=()=>{
     setI((i)=>(i+1)%a.length)
   }
@@ -26,25 +26,29 @@ let App=()=>{
   let upd=(ind)=>{
     setI((i)=>ind)
   }
-  useEffect(()=>{
-    let iid=setInterval(inc,3000)
-    return ()=>{
-      clearInterval(iid)
-    }
-  })
+  // useEffect(()=>{
+  //   let iid=setInterval(inc,3000)
+  //   return ()=>{
+  //     clearInterval(iid)
+  //   }
+  // })
 
   return(<div className="con">
+    <div className='imgs'>
+        <img src={a[i-1]}/>
         <img src={a[i]}/>
-        <h1 className="head">{h[i]}</h1>
-        <i class="fa-solid fa-less-than pre" onClick={dec}></i>
-        <i class="fa-solid fa-chevron-right nex" onClick={inc}></i>
-        <div className='dots'>
+        <img src={a[i+1]}/>
+        </div>
+        {/* <h1 className="head">{h[i+1]}</h1> */}
+        <i class={i>1?"fa-solid fa-less-than pre":""} onClick={dec}></i>
+        <i class={i<(a.length-2)?"fa-solid fa-chevron-right nex":""} onClick={inc}></i>
+        {/* <div className='dots'>
         {
           a.map((item,index)=>{
             return(<i className={index===i? "fa-solid fa-circle":"fa-regular fa-circle"} onClick={()=>upd(index)}></i>)
           })
         }
-        </div>
+        </div> */}
   </div>)
 }
 export default App
